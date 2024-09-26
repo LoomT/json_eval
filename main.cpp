56d27647417f0f5a9dd871f0f4d27f7ef5316d80
LoomT <kajustaida@gmail.com>
2024-09-26 09:56:13 +0200

    Updated main for debugging

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 using namespace std; // only std allowed anyway
 
 int main() {
-    hello();
+    unordered_map<string, Value> map = parseJSON("yep.txt");
+    cout << map.size() << endl;
+    for(auto &[key, value] : map) {
+        cout << key << " " << value.type << endl;
+    }
     return 0;
 }
