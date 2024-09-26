d3feb1e1512b5fa9a11a21fb211790082334ca0f
LoomT <kajustaida@gmail.com>
2024-09-26 09:54:07 +0200

    Custom parse exception

--- parse.h
+++ parse.h
@@ -1,6 +1,23 @@
 #ifndef JSON_EVAL_PARSE_H
 #define JSON_EVAL_PARSE_H
+#include <string>
+#include <unordered_map>
+#include "typeValuePair.h"
 
-void hello();
+class ParseException final : public std::exception {
+private:
+    std::string message;
+
+public:
+    explicit ParseException(const char* msg)
+        : message(msg) {}
+
+    [[nodiscard]] const char* what() const noexcept override
+    {
+        return message.c_str();
+    }
+};
+
+std::unordered_map<std::string, Value> parseJSON(const std::string& filePath);
 
 #endif //JSON_EVAL_PARSE_H
