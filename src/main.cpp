f009f89ee3609377bfd7dfe1b2fa5fd79eed2a5c
LoomT <kajustaida@gmail.com>
2024-10-04 07:55:12 +0200

    De-hardcode main

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,15 @@ using namespace std; // only std allowed anyway
 
 void printParsedExpression(unique_ptr<Node> node);
 
-int main(int argc, char* argv[]) {
-    const unordered_map<string, ValueJSON> map = parseJSON("yep.json");
-    const string input = "a[f][0]";
+int main(const int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: ./json_eval <json file> <expression>\n"
+                "Example: ./json_eval test.json \"a.b[1]\"" << endl;
+        return -1;
+    }
+    const unordered_map<string, ValueJSON> map = parseJSON(argv[1]);
+    const string input = argv[2];
+
     const unique_ptr<Node> result = parseExpression(input);
     cout << toString(executeExpression(map, *result)) << endl;
 
