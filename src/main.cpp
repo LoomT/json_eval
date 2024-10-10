a1c9e837bbb07675648eadee57979c97b3e87289
LoomT <kajustaida@gmail.com>
2024-10-10 15:25:52 +0200

    boxplot execution time analysis

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <chrono>
 #include <iostream>
+#include <numeric>
 
-#include "parseJSON.h"
-#include "expression.h"
 #include "JSON.h"
 
 using namespace std; // only std allowed anyway
@@ -13,19 +12,27 @@ int main(const int argc, char* argv[]) {
                 "Example: ./json_eval test.json \"a.b[1]\"" << endl;
         return -1;
     }
-    double totalTime = 0;
-    constexpr int runs = 25;
+    constexpr int runs = 5000;
+    auto times = array<double, runs>();
     for(int i = 0; i < runs; i++) {
         const auto start = chrono::steady_clock::now();
         const auto json = JSON(argv[1]);
         const string input = argv[2];
 
-        cout << toString(json.evaluate(input)) << endl;
+        toString(json.evaluate(input));
         const auto end = chrono::steady_clock::now();
         const chrono::duration<double, milli> diff = end - start;
-        totalTime += diff.count();
+        times[i] = diff.count();
     }
-    cout << "average execution time of " << runs << " runs: " << totalTime / runs << " ms" << endl;
+    sort(times.begin(), times.end());
+    cout << "Number of runs: " << runs << endl;
+    cout << "Fastest execution time: " << times[0] << " ms" << endl;
+    cout << "Faster quartile execution time: " << times[runs/4] << " ms" << endl;
+    cout << "Median execution time: " << times[runs/2] << " ms" << endl;
+    cout << "Slower quartile execution time: " << times[runs - runs/4 - 1] << " ms" << endl;
+    cout << "Slowest execution time: " << times[runs - 1] << " ms" << endl;
+    cout << "Average execution time: " << accumulate(times.begin(), times.end(), 0.0) / runs << " ms" << endl;
+    cout << "IQR: " << times[runs - runs/4 - 1] - times[runs/4] << " ms" << endl;
 
     return 0;
 }
