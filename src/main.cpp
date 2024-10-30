0ccdd003f60d0fa9b4e58f4a06a7d4367bd77718
LoomT <kajustaida@gmail.com>
2024-10-30 23:50:04 +0100

    Added ability to evaluate multiple expressions

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,25 @@ using namespace std; // only std allowed anyway
 int main(const int argc, char* argv[]) {
     if (argc != 3) {
         cout << "Usage: ./json_eval <json_file> <expression>\n"
+                "Or: ./json_eval -k <json_file>\n"
                 "Example: ./json_eval test.json \"a.b[1]\"" << endl;
         return -1;
     }
-    const auto json = JSON(argv[1]);
-    const string input = argv[2];
 
-    cout << toString(json.evaluate(input));
+    if (string(argv[1]) == "-k") {
+        const auto json = JSON(argv[2]);
+        do {
+            string input;
+            cin >> input;
+            if (input == "-x") break;
+            cout << toString(json.evaluate(input)) << endl;
+        } while (true);
+    } else {
+        const auto json = JSON(argv[1]);
+        const string input = argv[2];
+
+        cout << toString(json.evaluate(input));
+    }
 
     return 0;
 }
